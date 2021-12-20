5c69f90c4b58dc863fa37f090049ee75897ed448
Nazarii <ngolovatuy@gmail.com>
2021-12-20 21:54:42 +0100

    delte

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const webpackMerge = require("webpack-merge");
-const commonConfiguration = require("./webpack/common");
-
-module.exports = (_env, { mode }) => {
-    const enviromentConfig = require(`./webpack/${mode}`);
-	const mergedConfig = webpackMerge(commonConfiguration, enviromentConfig);
-	
-    return mergedConfig;
-};
